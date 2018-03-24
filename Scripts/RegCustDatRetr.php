905465d62fdd5cf667638168aec8dbba402e52ea
Shikhar <shikharndaa@gmail.com>
2018-03-23 20:34:26 -0700

    Registered customer php

---
+++ Scripts/RegCustDatRetr.php
@@ -0,0 +1,20 @@
+<?php
+
+$oci_Connector = OCILogon("ora_q5c1b", "a51931153", "ug")
+    ?>
+
+<html>
+<head>
+    <title> Display Prescriptions of the Registered Customer</title>
+    <style type = "text/css">
+        table 
+    </style>
+
+
+
+
+
+
+</head>
+
+</html>
