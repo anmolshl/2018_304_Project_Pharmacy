e0c7fd1d15fdba59107642c4cf0682ade9939959
Forrest <lljsg123123@gmail.com>
2018-03-26 15:46:02 -0700

    change pharmacist and prescription

--- Scripts/Pharmacist.php
+++ Scripts/Pharmacist.php
@@ -1,36 +1,83 @@
 <?php session_start();
 ?>
-/**
- * Created by PhpStorm.
- * User: lilinjian
- * Date: 25/03/2018
- * Time: 16:41
- */
-/**
- * Created by PhpStorm.
- * User: lilinjian
- * Date: 25/03/2018
- * Time: 15:18
- */
 <!DOCTYPE html>
 <html lang="en" style="background-color: beige">
 <head>
     <meta charset="UTF-8">
-    <title>Pharmtech - Pharmacist</title>
+    <title>Pharmtech - Prescription</title>
 </head>
 <body>
-<form action="Prescription.php">
-    <div class="container" align="center" style="margin-top: 20px;">
-        <button type="submit">Write Prescription</button>
-    </div>
-</form>
+    <form action="Prescription.php", method="post">
+        <div align="center" style="margin-bottom: 40px; margin-top: 20px; background-color: red">
+            <b style="font-family: 'American Typewriter'; font-size: 30px">PharmTech</b>
+        </div>
+        <div class="container" align="center" style="margin-top: 20px;">
+            <button type="submit">Write Prescription</button>
+        </div>
+    </form>
+    <form action="Pharmacist.php">
+        <div class="container" align="center" style="margin-top: 20px;">
+            <input type="number" placeholder="Prescription Number">
+            <button id="p_num" type="submit">Read Prescription</button>
+        </div>
+        <div class="container" align="center" style="margin-top: 20px;">
+            <input type="text" placeholder="Drug Name">
+            <button id="d_name" type="submit">Search Drug</button>
+        </div>
+        <div class="container" align="center" style="margin-top: 20px;">
+            <input type="number" placeholder="Stock ID">
+            <button id="s_id" type="submit">Check Stock</button>
+        </div>
+        <div class="container" align="center" style="margin-top: 20px;">
+            <input type="number" placeholder="Customer ID">
+            <button id="c_num" type="submit">Check Patient History</button>
+        </div>
+    </form>
 <?php
-
-$p_num = $_SESSION['p_num'];
-if ($p_num != 0){
-    echo $p_num;
+$conn = oci_connect("ora_q5c1b", "a51931153", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+if (!$conn) {
+    $m = oci_error();
+    echo $m['message'], "\n";
+    exit;
 }
-echo "bitch";
+else {
+    $p_num = $_GET('p_num');
+    //TODO: Get the information of such prescription
+    if($p_num != null) getPrescriptionInfo($p_num, $conn);
+    $d_name = $_GET('d_name');
+    if ($s_id != null)
+    //TODO: Get the information of such drug
+    $s_id = $_GET('s_id');
+    //TODO: Get the information of such stock
+    $c_num = $_GET('c_num');
+    //TODO: Get the information of such customer
+}
+
+function getPrescriptionInfo($p_num, $conn){
+    $p_info = "select * from Prescriotion where 'prescription number' = $p_num ";
+    $ociQuery = oci_parse($conn, $p_info);
+    oci_execute($ociQuery);
+    if($p_info != null) {
+        echo "<table border='1'>\n";
+        echo "<tr>\n";
+        echo "<td>Drug Name</td>\n";
+        echo "<td>Drug Type</td>\n";
+        echo "<td>Illness</td>\n";
+        echo "<td>Price</td>\n";
+        echo "</tr>\n";
+    }
+    while ($row = oci_fetch_array($ociQuery, OCI_ASSOC+OCI_RETURN_NULLS)) {
+        echo "<tr>\n";
+        foreach ($row as $item) {
+            echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+        }
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+
+}
+
+
 ?>
 </body>
 
--- Scripts/Prescription.php
+++ Scripts/Prescription.php
@@ -1,73 +1,83 @@
 <?php session_start();
 ?>
 /**
- * Created by PhpStorm.
- * User: lilinjian
- * Date: 25/03/2018
- * Time: 15:18
- */
+* Created by PhpStorm.
+* User: lilinjian
+* Date: 25/03/2018
+* Time: 15:18
+*/
+
 <!DOCTYPE html>
 <html lang="en" style="background-color: beige">
+
 <head>
     <meta charset="UTF-8">
-    <title>Pharmtech - Sign up</title>
+    <title>Pharmtech - Prescription</title>
 </head>
+
 <body>
-<form>
-    <div align="center" style="margin-bottom: 40px; margin-top: 20px; background-color: red">
-        <b style="font-family: 'American Typewriter'; font-size: 30px">PharmTech</b>
-    </div>
-    <div class="container" align="center">
-        <input id="s_loc" type="text" name="s_loc" placeholder="Store Location" required>
-    </div>
-    <div class="container" align="center">
-        <input id="u_name" type="text" name="u_name" placeholder="User Name" required>
-    </div>
-    <div class="container" align="center">
-        <input id="s_add" type="text" name="s_add" placeholder="Store Address" required>
-    </div>
-    <div class="container" align="center">
-        <input id="d_name" type="text" name="d_name" placeholder="Drug Name" required>
-    </div>
-    <div class="container" align="center">
-        <input id="refill" type="boolean" name="refill" placeholder="Refill?" required>
-    </div>
-    <div class="container" align="center">
-        <input id="expir" type="date" name="expir" placeholder="Expiration" required>
-    </div>
-    <div class="container" align="center">
-        <input id="c_num" type="number" name="c_num" placeholder="Customer Number" required>
-    </div>
-    <div class="container" align="center">
-        <input id="issued_d" type="date" name="issued_d" placeholder="Issued Date" required>
-    </div>
-    <div class="container" align="center">
-        <input id="dosage" type="number" name="dosage" placeholder="Dosage" required>
-    </div>
-</form>
+    <form action="Pharmacist.php" method="POST">
+        <div align="center" style="margin-bottom: 40px; margin-top: 20px; background-color: red">
+            <b style="font-family: 'American Typewriter'; font-size: 30px">PharmTech</b>
+        </div>
+        <div class="container" align="center">
+            <input id="refill" type="boolean" name="refill" placeholder="Refill?" required>
+        </div>
+        <div class="container" align="center">
+            <input id="expir" type="date" name="expir" placeholder="Expiration" required>
+        </div>
+        <div class="container" align="center">
+            <input id="c_num" type="number" name="c_num" placeholder="Customer Number" required>
+        </div>
+        <div class="container" align="center">
+            <input id="u_name" type="number" name="u_name" placeholder="Customer Name" required>
+        </div>
+        <div class="container" align="center">
+            <input id="issued_d" type="date" name="issued_d" placeholder="Issued Date" required>
+        </div>
+        <div class="container" align="center" style="margin-top: 20px;">
+            <button type="submit">Submit</button>
+        </div>
+    </form>
+
 </body>
+
 <?php
-    /*$s_loc = $_GET['s_loc'];
+$conn = oci_connect("ora_q5c1b", "a51931153", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+if (!$conn) {
+    $m = oci_error();
+    echo $m['message'], "\n";
+    exit;
+}
+else {
+
     $u_name = $_GET['u_name'];
-    $s_add = $_GET['s_add'];
-    $d_name = $_GET['d_name'];
     $refill = $_GET['refill'];
     $expir = $_GET['expir'];
     $c_num = $_GET['c_num'];
     $issued_d = $_GET['issued_d'];
-    $dosage = $_GET['dosage'];*/
-    $p_num = rand(10000,99999);
+    $p_num = rand(10000, 99999);
 
-    /*$prescriptionWrite = "insert into Prescription_orders(store_location, username, store_address, drug_name, refill,
-                                                              expiration, customer_number, prescription_number, issued_date, dosage) 
-                              VALUES ('".$s_loc."','".$_uname."','".$s_add."','".$d_name."','".$d_name."','".$refill."'
-                                        ,'".$expir."','".$c_num."','".$p_num."', '".$issued_d."', '".$dosage."')";*/
-    $_SESSION['p_num'] = $p_num;
+    $prescriptionWrite = "insert into Prescription(refill,expiration, customer_number, username, prescription_number, issued_date)
+                              VALUES ('".$refill."' ,'".$expir."','".$c_num."','".$_uname.",'".$p_num."', '".$issued_d."')";
+}
+
+/*$s_loc = $_GET['s_loc'];
+$u_name = $_GET['u_name'];
+$s_add = $_GET['s_add'];
+$d_name = $_GET['d_name'];
+$refill = $_GET['refill'];
+$expir = $_GET['expir'];
+$c_num = $_GET['c_num'];
+$issued_d = $_GET['issued_d'];
+$dosage = $_GET['dosage'];*/
+$p_num = rand(10000,99999);
+/*$prescriptionWrite = "insert into Prescription_orders(store_location, username, store_address, drug_name, refill,
+                                                          expiration, customer_number, prescription_number, issued_date, dosage)
+                          VALUES ('".$s_loc."','".$_uname."','".$s_add."','".$d_name."','".$d_name."','".$refill."'
+                                    ,'".$expir."','".$c_num."','".$p_num."', '".$issued_d."', '".$dosage."')";*/
+$_SESSION['p_num'] = $p_num;
+
 ?>
-<form action="Pharmacist.php" method="GET">
-    <div align="center" style="margin-bottom: 40px; margin-top: 20px; background-color: red">
-        <b style="font-family: 'American Typewriter'; font-size: 30px">PharmTech</b>
-    </div>
-    <input type="submit" value="submit">
-</form>
+
 </html>
