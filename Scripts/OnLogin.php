f8a2877674c5ea93b13f8516181e5ae3d40995b5
Shikhar <shikharndaa@gmail.com>
2018-03-26 18:12:05 -0700

    Some Login stuff

--- Scripts/OnLogin.php
+++ Scripts/OnLogin.php
@@ -14,3 +14,23 @@ $dom->loadHTMLFile("../Interfaces/LoginPage.html");
 $userName = $dom->getElementById("userName");
 $password = $dom->getElementById("password");
 
+
+$conn = oci_connect("ora_q5c1b", "a51931153", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+if (!$conn) {
+    $m = oci_error();
+    echo $m['message'], "\n";
+    exit;
+}
+else {
+    echo "<br>Connected to Oracle!</br>";
+    $userName2 = "select username from UserTab where drug_name='".$userName."'";
+    $password2 = "select password from UserTab where password='".$password."'";
+    $ociuserName2 = oci_parse($conn, $userName2);
+    $ocipassword2 = oci_parse($conn, $password2);
+    if ($password == $ocipassword2 && $userName == $ociuserName2) {
+        header("RegCustDatRetr.php");
+    }
+    else {
+        echo "Please try again, wrong username or password\n";
+    }
+    }
