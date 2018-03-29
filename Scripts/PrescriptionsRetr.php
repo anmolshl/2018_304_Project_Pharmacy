9d8522f30d6ae7ffab6b1c8feb22edab4cb6b213
Shikhar <shikharndaa@gmail.com>
2018-03-28 17:46:41 -0700

    PrescriptionsRetr finished

--- Scripts/PrescriptionsRetr.php
+++ Scripts/PrescriptionsRetr.php
@@ -31,17 +31,15 @@ if (!$conn) {
 }
 else {
     echo "<br>Connected to Oracle!</br>";
-    $PrescriptionQuery = "SELECT drug_name, refill, expiration, issued_date, dosage FROM Prescription_orders WHERE username = '" . $userName . "'";
+    $PrescriptionQuery = "SELECT expiration, prescription_number, issued_date FROM Prescription WHERE username = '".$userName."'";
     $ociQuery = oci_parse($conn, $PrescriptionQuery);
     oci_execute($ociQuery);
     selectQuery($conn, $ociQuery);
     echo "<table border='1'>\n";
     echo "<tr>\n";
-    echo "<td>Drug Name</td>\n";
-    echo "<td>Refill</td>\n";
     echo "<td>Expiration</td>\n";
+    echo "<td>Prescription_number</td>\n";
     echo "<td>Issued Date</td>\n";
-    echo "<td>Dosage</td>\n";
     while ($row = oci_fetch_array($ociQuery, OCI_ASSOC + OCI_RETURN_NULLS)) {
         echo "<tr>\n";
         foreach ($row as $item) {
